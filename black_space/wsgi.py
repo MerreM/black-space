7c539045ee0ee470a3a46ef5526fe8fbf6ffeafe
Michael Readhead <michael.readhead@virginwines.co.uk>
2014-04-23 11:54:19 +0100

    Shit to django.

---
+++ black_space/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for black_space project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "black_space.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
