328a1b8e97bfa9d9b6f65bf60d13dd3e50e3bb38
Ubuntu <ubuntu@ip-172-31-23-116.eu-west-1.compute.internal>
2014-09-08 12:41:29 +0000

    LCAL SETTINGS.

--- blackspace/settings/__init__.py
+++ blackspace/settings/__init__.py
@@ -2,6 +2,6 @@ from settings import *
 from pipeline import *
 from logging import *
 from cache import *
-
+from local import *
 if CACHING:
     MIDDLEWARE_CLASSES = ['django.middleware.cache.UpdateCacheMiddleware'] + MIDDLEWARE_CLASSES + ['django.middleware.cache.FetchFromCacheMiddleware']
