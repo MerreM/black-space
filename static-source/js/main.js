c8c7d894f329c4041614058161321afadb8d86a7
Michael Readhead <michael.readhead@virginwines.co.uk>
2014-09-12 14:58:37 +0100

    £d

--- static-source/js/main.js
+++ static-source/js/main.js
@@ -5,3 +5,24 @@ function fitToContainer(canvas){
   canvas.width  = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 }
+function init_box(){
+    var box = $('#box');
+    var showPanelButtons = $('#show-buttons button');
+    var panelClassName = 'show-front';
+    console.log(box)
+
+    function onButtonClick( event ){
+        box.removeClass( panelClassName );
+        panelClassName = event.target.className;
+        box.addClass( panelClassName );
+    };
+
+    for (var i=0, len = showPanelButtons.length; i < len; i++) {
+        console.log($(showPanelButtons[i]));
+        $(showPanelButtons[i]).on('click',onButtonClick);
+    }
+  
+    $('#toggle-backface-visibility').on( 'click', function(){
+        box.toggleClass('panels-backface-invisible');
+    }, false);
+}
