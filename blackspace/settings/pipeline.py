b22061b147b0f7d1a283b73da5e9f14a9dd7d757
Michael Readhead <michael.readhead@virginwines.co.uk>
2014-11-13 10:57:26 +0000

    Uglify?

--- blackspace/settings/pipeline.py
+++ blackspace/settings/pipeline.py
@@ -62,6 +62,6 @@ PIPELINE_COMPILERS = (
 )
 
 PIPELINE_CSS_COMPRESSOR = 'pipeline.compressors.yuglify.YuglifyCompressor'
-PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
+PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
 
 STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
