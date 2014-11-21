01ea4dc926419adeaa5742a69f72de9635e19565
Michael Readhead <michael.readhead@virginwines.co.uk>
2014-11-21 14:16:26 +0000

    Blur?

--- static-source/js/main.js
+++ static-source/js/main.js
@@ -1,9 +1,9 @@
 
 $(".latest_post").animo('blur', {duration: 3, amount: 15});
 
-$('.latest_post').mouseover(function(){$('.latest_post').animo('focus')});
+$(".latest_post").mouseover(function(){$('.latest_post').animo('focus')});
 
-$('.latest_post').mouseout(function(){$(".latest_post").animo('blur', {duration: 3, amount: 15})});
+$(".latest_post").mouseout(function(){$(".latest_post").animo('blur', {duration: 3, amount: 15})});
 
 $("#header-title").animo('blur', {duration: 3, amount: 1});
 
