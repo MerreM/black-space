1ddf74f807afdceb19d2d4fbd96441b414e63d62
Ubuntu <ubuntu@0xdb.co.uk>
2015-04-14 10:21:27 +0000

    Merges.

---
+++ civ_5_tracker/migrations/0014_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('civ_5_tracker', '0013_auto_20150402_1558'),
+        ('civ_5_tracker', '0013_auto_20150407_0917'),
+    ]
+
+    operations = [
+    ]
