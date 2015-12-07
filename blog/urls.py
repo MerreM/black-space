32bc40e8bcc64b9cd5327e8fff1b610e92edfb2e
Michael Readhead <michael.readhead@trackit-solutions.com>
2015-12-07 14:47:06 +0000

    Switeroo.

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import url
 
 from django.contrib import admin
 
@@ -9,8 +9,8 @@ urlpatterns = [
     url(r'^$', views.writing, name='writing'),
     url(r'^tags/(?P<tags>.+)/$', views.tags, name='tags'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^(?P<catergory>\w+)/$', views.catergory, name='catergory'),
-    url(r'^(?P<catergory>\w+)/(?P<slug>[-\w]+)/$', views.post, name='post'),
-    url(r'^(?P<catergory>\w+)/(?P<slug>[-\w]+)/(?P<percent>[\d]+)/$', views.read_post, name='read_post'),
-    url(r'^(?P<catergory>\w+)/(?P<slug>[-\w]+)/not_bad/$', views.liked_post, name='liked_post'),
+    url(r'^(?P<category>\w+)/$', views.category, name='category'),
+    url(r'^(?P<category>\w+)/(?P<slug>[-\w]+)/$', views.post, name='post'),
+    url(r'^(?P<category>\w+)/(?P<slug>[-\w]+)/(?P<percent>[\d]+)/$', views.read_post, name='read_post'),
+    url(r'^(?P<category>\w+)/(?P<slug>[-\w]+)/not_bad/$', views.liked_post, name='liked_post'),
 ]
