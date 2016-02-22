56dd968f6d6f2ee6e1857b63b80d9f7572fde2d0
Michael Readhead <michael.readhead@trackit-solutions.com>
2016-02-22 10:29:09 +0000

    More pep8.

--- civ_5_tracker/views.py
+++ civ_5_tracker/views.py
@@ -3,37 +3,38 @@ from django.shortcuts import get_object_or_404
 
 from civ_5_tracker.models import Game
 from civ_5_tracker.models import Player
-from civ_5_tracker.models import Victor
+# from civ_5_tracker.models import Victor
 
-# Create your views here.
+
 def games(request):
     games = Game.objects.all()
-    return render(request,"games.html",{"games":games})
+    return render(request, "games.html", {"games": games})
+
 
 def players(request):
     players = Player.objects.all()
     context = {
-        "players":players,
+        "players": players,
     }
-    return render(request,"players.html",context)
+    return render(request, "players.html", context)
 
-def player(request,player_name):
-    player = get_object_or_404(Player,name=player_name)
+
+def player(request, player_name):
+    player = get_object_or_404(Player, name=player_name)
     wins = player.victor_set.all()
     win_count = wins.count()
     games = player.participant_set.all()
     game_count = games.count()
     context = {
-        "player":player,
-        "wins":wins,
-        "win_count":win_count,
-        "game_count":game_count
+        "player": player,
+        "wins": wins,
+        "win_count": win_count,
+        "game_count": game_count
     }
-    return render(request,"player.html",context)
+    return render(request, "player.html", context)
 
 
 def victories(request):
     games = Game.objects.filter(victor__in=Player.objects.all())
-    players = Player.objects.all()
-    return render(request,"victories.html",{"games":games})
-
+    # players = Player.objects.all()
+    return render(request, "victories.html", {"games": games})
